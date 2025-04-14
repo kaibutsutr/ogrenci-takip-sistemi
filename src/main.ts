17885081a9e473724b0d2dacfe7bbdaf4b1e6f89
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2025-04-14 21:40:35 +0300

    1234

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
 const cookieSession = require('cookie-session'); // we need to write it like this, it has compatilability issues with TS
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
   app.use(
     // activate cookie-section
     cookieSession({
@@ -16,9 +21,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true }), // we want validation pipe for all requests
   );
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap(); //main entry
+bootstrap();
