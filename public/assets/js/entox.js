2aa4b0151dc826c54675047e922836b3dd10f1f1
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2025-04-14 23:49:27 +0300

    123

--- public/assets/js/entox.js
+++ public/assets/js/entox.js
@@ -2154,17 +2154,36 @@ async function signUpUser() {
 
 /*------------ Login ---------*/
 async function signInUser(loginEmail, loginPassword) {
-  const { data } = await axios.post(
-    'http://localhost:3000/auth/signin',
-    {
-      email: loginEmail,
-      password: loginPassword,
-    },
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+  const { data } = await axios
+    .post(
+      'http://localhost:3000/auth/signin',
+      {
+        email: loginEmail,
+        password: loginPassword,
       },
-    },
-  );
-  console.log(data);
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    )
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        alert(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        alert('Error', error.message);
+      }
+      console.log(error.config);
+    });
+  window.location.href = 'hesabim.html';
 }
