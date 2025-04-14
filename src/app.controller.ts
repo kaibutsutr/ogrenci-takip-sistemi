69e0979863217008a197ded5ada675112dd97f04
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2025-04-14 21:47:23 +0300

    121

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Get, Controller, Res, Render } from '@nestjs/common';
+import { Response } from 'express';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private appService: AppService) {}
+
   @Get()
-  @Render('index')
-  root() {
-    return { message: 'Hello world!' };
+  root(@Res() res: Response) {
+    return res.render(this.appService.getViewName(), {
+      message: 'Hello world!',
+    });
   }
 }
