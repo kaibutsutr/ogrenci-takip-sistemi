8efe4ef49753677998b5f1960bee74e8cd7da0f3
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2025-04-30 23:09:31 +0300

    fk

--- public/assets/js/entox.js
+++ public/assets/js/entox.js
@@ -2897,39 +2897,40 @@ async function signUpUser(
 }
 /*------------ Logout ---------*/
 async function signOutUser() {
-  const { data } = await axios
-    .post(
-      'http://localhost:3000/auth/signout',
-      {},
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      },
-    )
-    .catch(function (error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-        alert(error.response.data.message);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Beklenmeyen bir hata oluştu', error.message);
-      }
-      console.log(error.config);
-    });
-  localStorage.removeItem('name');
-  localStorage.removeItem('surname');
-  console.log('signout successful');
-  window.location.href = 'giris.html';
+   localStorage.removeItem('name');
+   localStorage.removeItem('surname');
+   console.log('signout successful');
+   window.location.href = 'giris.html';
+   const { data } = await axios
+     .post(
+       'http://localhost:3000/auth/signout',
+       {},
+       {
+         headers: {
+           'Content-Type': 'application/x-www-form-urlencoded',
+         },
+       },
+     )
+     .catch(function (error) {
+       if (error.response) {
+         // The request was made and the server responded with a status code
+         // that falls out of the range of 2xx
+         console.log(error.response.data);
+         console.log(error.response.status);
+         console.log(error.response.headers);
+         alert(error.response.data.message);
+       } else if (error.request) {
+         // The request was made but no response was received
+         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+         // http.ClientRequest in node.js
+         console.log(error.request);
+       } else {
+         // Something happened in setting up the request that triggered an Error
+         console.log('Beklenmeyen bir hata oluştu', error.message);
+       }
+       console.log(error.config);
+     });
+ 
 
   //axios signout
 }
